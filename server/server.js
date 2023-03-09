aed83704b327602fab852127f8a392cfba120dc5
Brooklyn Longhurst <brooklyn.longhurst@gmail.com>
2023-03-09 13:21:04 -0700

    inital commit for code along

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+//endpoints
+
+const { createMessage } = require('./controller')
+
+app.post('/api/messages', createMessage)
+
+
+
+app.listen(4004,() => console.log('server is running on 4004'))
